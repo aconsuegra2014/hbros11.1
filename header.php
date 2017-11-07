1e96e080f807543725135c5d7a6b8aa8d4142c65
Arley Consuegra Roselló <aconsuegra@cmkx.icrt.cu>
2017-11-07 12:38:01 -0500

    Agregando meta etiqueta que me pone como dueño del sitio. Mi correo es aconsuegra2014@gmail.com

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <title><?php bloginfo('name'); ?></title>
      <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="google-site-verification" content="r-VDsjyVuLh9T70QG8SlznY_fOTqlehAc3QSXWfcEAs" />
     <!-- Bootstrap -->
     <link href="<?php bloginfo('template_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	<!-- FontAwesome -->
