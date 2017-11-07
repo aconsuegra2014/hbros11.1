1be5ae437dc993cd289c8fa7723da2d228fb265a
Arley Consuegra Roselló <aconsuegra@cmkx.icrt.cu>
2017-11-07 10:40:37 -0500

    Agregando menu superior con soporte para boostrap 4

--- functions.php
+++ functions.php
@@ -20,3 +20,33 @@ function  wpc_custom_excerpt( $excerpt ) {
 add_filter ( 'the_excerpt' ,   'wpc_custom_excerpt' ) ;
 */
 
+/* Adding support for menu */
+function register_my_menu() {
+  register_nav_menu('top-menu',__( 'Top menu' ));
+}
+add_action( 'init', 'register_my_menu' );
+
+// Register Custom Navigation Walker
+    require_once 'D:\wamp\www\wordpress\wp-content\themes\AconsuegraCmk\wp-bootstrap-navwalker.php';
+
+// Adding class to links in top menu	
+function add_specific_link_class( $atts, $item, $args ) {
+    // check if the item is in the top menu
+    if( $args->theme_location == 'top-menu' ) {
+      // add the desired attributes:
+      $atts['class'] = 'nav-link';
+    }
+    return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'add_specific_link_class', 10, 3 );	
+
+// Adding class to li items in top menu	
+function add_specific_li_class( $classes, $item, $args ) { 
+      if( $args->theme_location == 'top-menu' ) {
+	    $classes = array('nav-item');
+	  }
+	  return $classes;
+}
+
+add_filter( 'nav_menu_css_class', 'add_specific_li_class', 10, 4 ); 
+
--- header.php
+++ header.php
@@ -33,50 +33,32 @@
 		  <span>99.5 FM / 1150 AM</span>
 		</div>
 	  </div>
-    <div class="row topNavBar"> 	  
+	  
+
+	  <div class="row topNavBar"> 	  
 	  <div class="col-md-12">
-	  <nav class="navbar navbar-expand-lg navBarLight">
+	  <nav class="navbar navbar-expand-lg navBarLight navbar-light">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
-        </li>
-       <li class="nav-item">
-          <a class="nav-link" href="#">Noticias</a>
-        </li>
-	    <li class="nav-item">
-          <a class="nav-link" href="#">Entrevistas</a>
-        </li>
-	    <li class="nav-item">
-          <a class="nav-link" href="#">Opinión</a>
-        </li>
-	  	    <li class="nav-item">
-         <a class="nav-link" href="#">Granma</a>
-        </li>
-	    <li class="nav-item">
-          <a class="nav-link" href="#">Cuba</a>
-          </li>
-	      <li class="nav-item">
-            <a class="nav-link" href="#">Mundo</a>
-         </li>
-	     <li class="nav-item">
-           <a class="nav-link" href="#">Archivo</a>
-         </li>
-        </ul>
+     
+      <?php
+            wp_nav_menu( array(
+                'menu'              => 'top-menu',
+                'theme_location'    => 'top-menu',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'navbarSupportedContent',
+                'menu_class'        => 'navbar-nav mr-auto',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker())
+            );
+        ?>
 	  <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
+          <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Buscar">
+	  </form>
+     
     </nav>
     </div>
   </div>
-      
-	  
-	
-	
-  
-  
