536007baa4a2a2b6e4a974fef6977c41aeeaf1c5
Arley Consuegra Roselló <aconsuegra2014@gmail.com>
2017-11-17 14:11:06 -0500

    Adding pagiantion to category.php

--- category.php
+++ category.php
@@ -1,51 +1,40 @@
 <?php get_header(); ?>
 
 <div class="row">
-  <div class="col-md-9">
-		<?php if ( have_posts() ) : ?>
-		<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentythirteen' ), single_cat_title( '', false ) ); ?></h1>
-		<?php if ( category_description() ) : // Show an optional category description ?>
-          <div class="archive-meta"><?php echo category_description(); ?></div>
+    <div class="col-md-12">
+	<?php if ( have_posts() ) : ?>
+	    <h2><?php single_cat_title( '', true ); ?></h2>
+	    <?php if ( category_description() ) : // Show an optional category description ?>
+		<div><?php echo category_description(); ?></div>
+	    <?php endif; ?>
+	    <?php $colCounter = 0; ?>
+	    <?php while ( have_posts() ) : the_post(); ?>
+		<?php if( $colCounter  == 0) : ?>
+		    <?php echo '<div class="row">'; ?>
 		<?php endif; ?>
-	
-		<?php while ( have_posts() ) : the_post(); ?>
-		<div id="bloquearchivo">
-		<div id="cattag" >
-    	<span class="categories"><?php _e('', 'ejemplo'); ?> <?php the_category(', '); ?></span>
-		<?php the_tags('<span class="tags"> <span class="sep">|</span> ' . __('', 'ejemplo') . ' ', ', ', '</span>'); ?>									
+		<div class="col-md-3">
+		    <div class="categoryImagePostsContainer">
+			<?php the_post_thumbnail(); ?>
+		    </div>
+		    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 		</div>
-		<div id="principal_container_title">
-		<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-		</div >
-	    <?php
-        // Get the ID of a given category
-        $category_id = get_cat_ID( 'Category Name' );
-
-        // Get the URL of this category
-        $category_link = get_category_link( $category_id );
-?>
-
-<!-- Print a link to this category -->
-<a href="<?php echo esc_url( $category_link ); ?>" title="Category Name">Category Name</a><?php
-    // Get the ID of a given category
-    $category_id = get_cat_ID( 'Category Name' );
-
-    // Get the URL of this category
-    $category_link = get_category_link( $category_id );
-?>
-
-<!-- Print a link to this category -->
-<a href="<?php echo esc_url( $category_link ); ?>" title="Category Name">Category Name</a>								
-
-					<div id="principal_container_texto">
-						<?php the_excerpt() ; ?>										
-					</div > 
-				</div>	
-			<?php endwhile; ?>			
+		<?php if( $colCounter == 3  ) : ?>
+		    <?php echo '</div>'; ?>
+		    <?php $colCounter = 0; ?>
+		<?php  else: ?>
+		    <?php $colCounter++; ?>
 		<?php endif; ?>
-  </div>
-  <div class="col-md-3">
-  </div>
+	    <?php endwhile; ?>
+	    <?php if($colCounter >= 1) : ?>
+		<?php echo '</div>'; ?>
+	    <?php endif; ?>
+	<?php endif; ?>
+    </div>
 </div>
-	
+
+<div class="row">
+    <div class="col-md-12">
+	<?php the_posts_pagination( array( 'mid_size' => 2, 'next_text' => 'Siguiente', 'prev_text' => 'Anterior', 'screen_reader_text' => 'a', 'type' => 'list' ) ); ?>
+    </div>
+</div>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 /*
    Remove Unwanted Links and Tags From WordPress Head
-*/
+ */
 
 // remove the meta generator WP version tag
 remove_action( 'wp_head', 'wp_generator' );
@@ -15,8 +15,8 @@ remove_action( 'wp_head', 'wlwmanifest_link' );
 
 function additional_js()
 {
-  wp_enqueue_script('jquery');
-  wp_print_scripts();
+    wp_enqueue_script('jquery');
+    wp_print_scripts();
 
 
 }
@@ -27,30 +27,30 @@ add_theme_support( 'post-thumbnails' );
 
 /* Add custom class to the_excerpt method */
 /*
-function  wpc_custom_excerpt( $excerpt ) {
- $excerpt  =  str_replace (  '<p' ,  '<p class = "clamp"' ,  $excerpt  ) ; 
- return  $excerpt ; 
-} 
-add_filter ( 'the_excerpt' ,   'wpc_custom_excerpt' ) ;
-*/
+   function  wpc_custom_excerpt( $excerpt ) {
+   $excerpt  =  str_replace (  '<p' ,  '<p class = "clamp"' ,  $excerpt  ) ; 
+   return  $excerpt ; 
+   } 
+   add_filter ( 'the_excerpt' ,   'wpc_custom_excerpt' ) ;
+ */
 
 /* Adding support for menu */
 function register_my_menu() {
-  register_nav_menus( array('top-menu' =>   'Top menu',
-                             'side-menu' => 'Side menu'
-							 ));
+    register_nav_menus( array('top-menu' =>   'Top menu',
+                              'side-menu' => 'Side menu'
+    ));
 }
 add_action( 'init', 'register_my_menu' );
 
 // Register Custom Navigation Walker
-    require_once 'D:\wamp\www\wordpress\wp-content\themes\AconsuegraCmk\wp-bootstrap-navwalker.php';
+require_once 'D:\wamp\www\wordpress\wp-content\themes\AconsuegraCmk\wp-bootstrap-navwalker.php';
 
 // Adding class to links in top menu	
 function add_specific_link_class( $atts, $item, $args ) {
     // check if the item is in the top menu
     if( $args->theme_location == 'top-menu' ) {
-      // add the desired attributes:
-      $atts['class'] = 'nav-link';
+	// add the desired attributes:
+	$atts['class'] = 'nav-link';
     }
     return $atts;
 }
@@ -58,23 +58,33 @@ add_filter( 'nav_menu_link_attributes', 'add_specific_link_class', 10, 3 );
 
 // Adding class to li items in top menu	
 function add_specific_li_class_to_top_menu( $classes, $item, $args ) { 
-      if( $args->theme_location == 'top-menu' ) {
-	    $classes = array('nav-item');
-	  }
-	  return $classes;
+    if( $args->theme_location == 'top-menu' ) {
+	$classes = array('nav-item');
+    }
+    return $classes;
 }
 
-add_filter( 'nav_menu_css_class', 'add_specific_li_class_to_top_menu', 10, 4 ); 
+add_filter( 'nav_menu_css_class', 'add_specific_li_class_to_top_menu', 10, 3 ); 
 
 // Adding class to li items in side menu	
 function add_specific_li_class_to_side_menu( $classes, $item, $args ) { 
-      if( $args->theme_location == 'side-menu' ) {
-	    $classes = array('list-group-item');
-	  }
-	  return $classes;
+    if( $args->theme_location == 'side-menu' ) {
+	$classes = array('list-group-item');
+    }
+    return $classes;
 }
 
-add_filter( 'nav_menu_css_class', 'add_specific_li_class_to_side_menu', 10, 4 ); 
+add_filter( 'nav_menu_css_class', 'add_specific_li_class_to_side_menu', 10, 3 );
+
+// Remove screen text reader in post navigation (pagination)
+function remove_screen_reader_text_post_navigation(){
+	$template= '<nav class="post-navigation %1$s" role="navigation">
+	<div class="nav-links">%3$s</div>
+	</nav>';
+
+    return $template;
+}
+add_filter( 'navigation_markup_template', 'remove_screen_reader_text_post_navigation', 10, 0 );
 
 // Set post count visit metadata
 function set_popular_post($postID) {
@@ -93,12 +103,12 @@ function set_popular_post($postID) {
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 // Counting popular post
 function count_popular_post($post_id) {
-	if (!is_single()) return;
-	if (empty($post_id)) {
-		global $post;
-		$post_id = $post->ID;
-	}
-	set_popular_post($post_id);
+    if (!is_single()) return;
+    if (empty($post_id)) {
+	global $post;
+	$post_id = $post->ID;
+    }
+    set_popular_post($post_id);
 }
 // Inserting 'count_popular_post' function in head of single posts.
 add_action( 'wp_head', 'count_popular_post');
